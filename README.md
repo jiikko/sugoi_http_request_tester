0dfade7276629a42ffda268f97fdaa074f5b3a61
jiikko <n905i.1214@gmail.com>
2016-07-21 21:58:02 +0900

    fix

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # SugoiHttpRequestTester
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/sugoi_http_request_tester`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
-
 ## Installation
 
 Add this line to your application's Gemfile:
@@ -21,16 +17,19 @@ Or install it yourself as:
     $ gem install sugoi_http_request_tester
 
 ## Usage
-
-TODO: Write usage instructions here
-
-## Development
-
-After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
-
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
+```ruby
+tester = SugoiHttpRequestTester.new(
+  host: 'example.com',
+  limit: 100,
+  basic_auth: [ENV['OUTING_BASIC_AUTH_USER'], ENV['OUTING_BASIC_AUTH_PASSWORD']],
+  logs_path: 'logs/*',
+) do |line|
+  /({.*})/ =~ line
+  JSON.parse($1)
+end
+tester.run
+```
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/sugoi_http_request_tester.
-
+Bug reports and pull requests are welcome.
--- lib/sugoi_http_request_tester.rb
+++ lib/sugoi_http_request_tester.rb
@@ -1,5 +1,87 @@
 require "sugoi_http_request_tester/version"
+require 'json'
+require 'net/http'
 
 module SugoiHttpRequestTester
-  # Your code goes here...
+  class Runner
+    def initialize(host: , limit: nil, basic_auth: nil, logs_path: nil, block: nil)
+      @host = host
+      @accessed_list = []
+      @manual_list = []
+      @limit = limit
+      @basic_auth = basic_auth
+      @logs_path = logs_path
+      @line_parser_block =
+        if block_given?
+          block
+        else
+          ->(line) {
+            /({.*})/ =~ line
+            yield JSON.parse($1)
+          }
+        end
+    end
+
+  def run
+    lines.each do |hash|
+      if /GET/ =~ hash['mt']
+        Net::HTTP.start(@host) do |http|
+          req = Net::HTTP::Get.new(hash['pt'])
+          req.basic_auth *@basic_auth unless @basic_auth.nil?
+          response = http.request(req)
+          add_result(to: :accessed_list, hash: hash, code: response.code)
+        end
+      else
+        add_result(to: :manual_list, hash: hash)
+      end
+    end
+    export
+  end
+
+  private
+
+  def export
+    File.write('accessed_list', @accessed_list.join("\n"))
+    File.write('manual_list', @manual_list.join("\n"))
+  end
+
+  # 圧縮されたファイルをここで読むとメモリ食うので解凍済みを使う
+  def lines
+    [].tap do |array|
+      Dir.glob(@logs_path).each do |file_name|
+        next if /\.gz$/ =~ file_name
+        File.open(file_name).each_line do |line|
+          array << @line_parser_block.call(line)
+          break if countup_and_limit?
+        end
+      end
+    end
+  end
+
+  def add_result(to: , hash: , code: nil)
+    case to
+    when :accessed_list
+      puts code
+      @accessed_list << [hash, code].join(',')
+    when :manual_list
+      @manual_list << hash
+    else
+      raise 'bug!!'
+    end
+  end
+
+  def countup_and_limit?
+    if @limit_counter.nil? && @limit.is_a?(Numeric)
+      @limit_counter = 0
+    end
+    if @limit.is_a?(Numeric)
+      @limit_counter += 1
+      @limit_counter > @limit
+    end
+  end
+end
+
+  def self.new(host:, limit: nil, basic_auth: nil, logs_path:, &block)
+    Runner.new(host: host, limit: limit, basic_auth: basic_auth, logs_path: logs_path, block: block)
+  end
 end
