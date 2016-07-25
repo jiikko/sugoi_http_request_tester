266aed5212fc38e8fbe53d3a63de29f1bdbab7c0
jiikko <n905i.1214@gmail.com>
2016-07-25 21:59:18 +0900

    fix spec

--- spec/sugoi_http_request_tester_spec.rb
+++ spec/sugoi_http_request_tester_spec.rb
@@ -30,6 +30,33 @@ describe SugoiHttpRequestTester do
       tester.run
       expect(tester.instance_eval { @request_list.size }).to eq 3
     end
+
+    it 'manual_list を出力していること' do
+      log = <<-LOG
+{"mt":"GET","pt":"/index.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index3.html","ua":"ddd"}
+{"mt":"POST","pt":"/index3.html","ua":"ddd"}
+      LOG
+      File.write('spec/logs/log1', log)
+      tester = SugoiHttpRequestTester.new(
+        host: 'example.com',
+        limit: 100,
+        basic_auth: [ENV['OUTING_BASIC_AUTH_USER'], ENV['OUTING_BASIC_AUTH_PASSWORD']],
+        logs_path: 'spec/logs/*',
+        concurrency: 3,
+      )
+      tester.set_line_parse_block = ->(line){
+        /({.*})/ =~ line
+        json = JSON.parse($1)
+        { method: json['mt'], user_agent: json['ua'], path: json['pt'] }
+      }
+      tester.import_logs
+      tester.run
+      binding.pry
+      expect(File.open(SugoiHttpRequestTester::EXPORT_MANUAL_LIST_PATH).readlines.size).to eq 1
+    end
   end
 
   describe 'limit' do
