a0d321e30390d0099ee7d3b5870fb15c4505eff8
jiikko <n905i.1214@gmail.com>
2016-08-04 00:29:39 +0900

    add spec

--- spec/sugoi_http_request_tester_spec.rb
+++ spec/sugoi_http_request_tester_spec.rb
@@ -6,6 +6,61 @@ describe SugoiHttpRequestTester do
   end
 
   describe '#run' do
+    describe '::OutputFormat::Array' do
+      context 'when sequential' do
+        it 'be array' do
+          log = <<-LOG
+{"mt":"GET","pt":"/index.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index3.html","ua":"ddd"}
+          LOG
+          File.write('spec/logs/log1', log)
+          tester = SugoiHttpRequestTester.new(
+            host: 'example.com',
+            limit: 100,
+            basic_auth: [ENV['OUTING_BASIC_AUTH_USER'], ENV['OUTING_BASIC_AUTH_PASSWORD']],
+            logs_path: 'spec/logs/*',
+            concurrency: 3,
+          )
+          tester.set_line_parse_block = ->(line){
+            /({.*})/ =~ line
+            json = JSON.parse($1)
+            { method: json['mt'], user_agent: json['ua'], path: json['pt'] }
+          }
+          tester.import_logs
+          array = tester.run(output_format: :array)
+          expect(array.size).to eq 3
+        end
+      end
+      context 'when sequential' do
+        it 'be array' do
+          log = <<-LOG
+{"mt":"GET","pt":"/index.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index2.html","ua":"ddd"}
+{"mt":"GET","pt":"/index3.html","ua":"ddd"}
+          LOG
+          File.write('spec/logs/log1', log)
+          tester = SugoiHttpRequestTester.new(
+            host: 'example.com',
+            limit: 100,
+            basic_auth: [ENV['OUTING_BASIC_AUTH_USER'], ENV['OUTING_BASIC_AUTH_PASSWORD']],
+            logs_path: 'spec/logs/*',
+            concurrency: 1,
+          )
+          tester.set_line_parse_block = ->(line){
+            /({.*})/ =~ line
+            json = JSON.parse($1)
+            { method: json['mt'], user_agent: json['ua'], path: json['pt'] }
+          }
+          tester.import_logs
+          array = tester.run(output_format: :array)
+          expect(array.size).to eq 3
+        end
+      end
+    end
+
     it 'be to set @request_list' do
       log = <<-LOG
 {"mt":"GET","pt":"/index.html","ua":"ddd"}
